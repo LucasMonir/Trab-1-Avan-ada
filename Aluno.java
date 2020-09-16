679bce4f384fec1c3eb8a854e3602d7c3fcca410
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-09-16 20:42:09 -0300

    Add files via upload

---
+++ Aluno.java
@@ -0,0 +1,6 @@
+public class Aluno extends Pessoa{
+
+    public Aluno(String nome, int cpf, int matricula, String materia, String turno) {
+        super(nome, cpf, matricula, materia, turno);
+    }
+}
