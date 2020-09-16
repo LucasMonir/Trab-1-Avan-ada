52e9dc1b2bfdbc1469b989198b95c4e9471d5c70
Ian <iankkles@gmail.com>
2020-09-16 20:52:25 -0300

    Revert "Commit_1"
    This reverts commit 6e6bfc442a1e234118b6474ea3f1154a0a54533c.

--- Professor.java
+++
@@ -1,6 +0,0 @@
-public class Professor extends Pessoa{
-
-    public Professor(String nome, int cpf, int matricula, String materia, String turno) {
-        super(nome, cpf, matricula, materia, turno);
-    }
-}
