e6b7aab0259f01398fb2cc03b339f815945b9314
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-09-18 14:39:15 -0300

    Action events substituidos
    actionEvents foram movidos pro mouseEvent pois foi adicionado o efeito hover

--- AlunoTable.java
+++ AlunoTable.java
@@ -51,34 +51,15 @@ public class AlunoTable extends JFrame {
 
         //botão de registrar e ação de registrar
         JButton registrar = new JButton("Registrar");
+        
+        //cor do jbutton
+        Color cor = registrar.getBackground();
 
-        registrar.addActionListener(new ActionListener() {
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                ptm.addPessoa(registraPessoas(nome.getText(), Integer.parseInt(cpf.getText()), Integer.parseInt(matricula.getText()), materia.getText(), turno.getText()));      
-                cleaner();
-            }
-            
-        });
-
-        //botao de editar, depois de alterar campo, double click salva progresso
-        JButton editar = new JButton("Editar");
-
-        editar.addMouseListener(new MouseListener() {
+        registrar.addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                if(e.getClickCount()==2){
-                    ptm.getPessoa(alunoTable.getSelectedRow()).setNome((nome.getText()));
-                    ptm.getPessoa(alunoTable.getSelectedRow()).setCpf(Integer.parseInt(cpf.getText()));
-                    ptm.getPessoa(alunoTable.getSelectedRow()).setMatricula(Integer.parseInt(matricula.getText()));
-                    ptm.getPessoa(alunoTable.getSelectedRow()).setMateria(materia.getText());
-                    ptm.getPessoa(alunoTable.getSelectedRow()).setTurno(turno.getText());
-
-                    ptm.fireTableDataChanged();
-                    
-                    cleaner();
-                }
+                ptm.addPessoa(registraPessoas(nome.getText(), Integer.parseInt(cpf.getText()), Integer.parseInt(matricula.getText()), materia.getText(), turno.getText()));      
+                cleaner();
             }
 
             @Override
@@ -95,24 +76,83 @@ public class AlunoTable extends JFrame {
 
             @Override
             public void mouseEntered(MouseEvent e) {
+                registrar.setBackground(Color.CYAN);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                registrar.setBackground(cor);
+            }
+        });
+
+        //botao de editar, depois de alterar campo, double click salva progresso
+        JButton editar = new JButton("Editar");
+
+        editar.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                ptm.getPessoa(alunoTable.getSelectedRow()).setNome((nome.getText()));
+                ptm.getPessoa(alunoTable.getSelectedRow()).setCpf(Integer.parseInt(cpf.getText()));
+                ptm.getPessoa(alunoTable.getSelectedRow()).setMatricula(Integer.parseInt(matricula.getText()));
+                ptm.getPessoa(alunoTable.getSelectedRow()).setMateria(materia.getText());
+                ptm.getPessoa(alunoTable.getSelectedRow()).setTurno(turno.getText());
+                ptm.fireTableDataChanged();
+                    
+                cleaner();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
                 // TODO Auto-generated method stub
 
             }
 
             @Override
-            public void mouseExited(MouseEvent e) {
+            public void mouseReleased(MouseEvent e) {
                 // TODO Auto-generated method stub
 
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                editar.setBackground(Color.CYAN);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                editar.setBackground(cor);
             }
         });
 
         //limpa os campos
         JButton cancelar = new JButton("Cancelar");
 
-        cancelar.addActionListener(new ActionListener() {
+        cancelar.addMouseListener(new MouseListener() {
             @Override
-            public void actionPerformed(ActionEvent arg0) {
+            public void mouseClicked(MouseEvent e) {
                 cleaner();
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                cancelar.setBackground(Color.CYAN);
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                cancelar.setBackground(cor);
             }
         });
 
