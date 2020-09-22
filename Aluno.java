94d1ea04e4231ffeec06708e87e5612f4c9d02b9
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-09-22 11:03:27 -0300

    Delete Aluno.java

--- Aluno.java
+++
@@ -1,6 +0,0 @@
-public class Aluno extends Pessoa{
-
-    public Aluno(String nome, int cpf, int matricula, String materia, String turno) {
-        super(nome, cpf, matricula, materia, turno);
-    }
-}
