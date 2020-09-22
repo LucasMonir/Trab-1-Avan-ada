d1be1e6aa24aa860ba7626b41a8dc076c49a116e
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-09-22 11:09:02 -0300

    Import e package adicionados

--- Aluno/Aluno.java
+++ Aluno/Aluno.java
@@ -1,3 +1,6 @@
+package Aluno;
+import Pessoa.*;
+
 public class Aluno extends Pessoa{
     public Aluno(String nome, int cpf, int matricula, String materia, String turno) {
         super(nome, cpf, matricula, materia, turno);
